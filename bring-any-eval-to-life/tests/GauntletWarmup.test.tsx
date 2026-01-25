5d77e4606140fb68e67a5c3b332ca922b67660f2
surfiniaburger <jdmasciano2@gmail.com>
2026-01-25 02:15:29 +0100

    refactor: address code review feedback for gauntlet warmup

--- bring-any-eval-to-life/tests/GauntletWarmup.test.tsx
+++ bring-any-eval-to-life/tests/GauntletWarmup.test.tsx
@@ -3,7 +3,6 @@ import { WARMUP_TARGET_X } from '../lib-web/gauntlet-constants';
 
 describe('Gauntlet Warmup Configuration', () => {
     it('should have the warmup target centered at x=0', () => {
-        // This test will fail initially because WARMUP_TARGET_X is -10
         expect(WARMUP_TARGET_X).toBe(0);
     });
 });
